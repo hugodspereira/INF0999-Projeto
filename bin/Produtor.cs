178afc53052c35f3667bde69470f7335f0486465
Hugo de Siqueira Pereira <msft31@gomes.LABS-IC>
2022-12-17 17:20:39 -0300

    Tentando criar Json

---
+++ bin/Produtor.cs
@@ -0,0 +1,17 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using Microsoft.Win32;
+
+
+namespace INF0999_Projeto
+{
+    public class Produtor
+    {
+        
+    }
+
+
+}
