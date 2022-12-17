12158f73cbc1ff719240c211712b9ea81cd0c2c5
carlosmilani <cmilanixavier@gmail.com>
2022-12-17 11:06:13 -0300

    commit 1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System.Windows;
+
+namespace INF0999_Projeto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
